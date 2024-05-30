7a1d50df8d98829e68ca22ed9c2b9fffcff6f4e9
WillssEdgar <willssedgar2123@gmail.com>
2024-05-29 20:48:44 -0400

    initial commit

---
+++ VSCodium/User/History/5af9c286/s29m.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column( 
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [ 
+      Text( "Hello World",
+          style: TextStyle(color: Colors.red),
+        ),
+      Text("Text"),
+      ],
+    );
+  }
+}
